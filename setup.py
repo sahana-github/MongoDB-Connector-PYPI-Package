1b32ae05baf5d210c2c5a84ccc54250ce9198914
Sahana <1ms20is407@gmail.com>
2024-10-23 06:59:42 +0530

    Your commit message

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "MongoDB-Connector-PYPI-Package"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "sahana-github"
+AUTHOR_EMAIL = "1ms20is407@gmail.com"
 
 setup(
     name=PKG_NAME,
